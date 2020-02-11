ef11b2e99479ee4b8114b92f244211febb783284
root <root@LAPTOP-D4I8D0R7.localdomain>
2020-02-11 20:06:29 +0000

    V2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var button = document.getElementById("button-yellow");
 button.onclick = function(){
     window.prompt("Join Our Mailing List");
-}
+    alert("Thank you!");
+} 
 console.log("Join Us" + button);
